53df2150dd49d811de23e9f4a56b3d920e040eeb
Dimitar Kostov <dimitar.kostov@158ltd.com>
2016-04-24 11:28:41 +0300

    messages

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -3,10 +3,12 @@ import Firebase from 'firebase';
 angular.module('superLiveChat')
 .controller('homeController', function($scope) {
   const myFirebaseRef = new Firebase(FIREBASE_URL);
+  $scope.messages = [];
   myFirebaseRef.set({
     message: 'Hello World'
   });
   myFirebaseRef.on('value', function(snapshot) {
     console.log(snapshot.val());
+    $scope.messages.push(snapshot.val());
   });
 });
